4b6041924e2d400fdef76f30bfcf11b9eb8c7970
Luc van den Enden <luc.vandenenden@gmail.com>
2022-07-12 11:25:29 +0200

    Added css loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,34 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/index.ts",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
-        library: 'TerminalEmulatorLib',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?(ts|tsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "@babel/preset-typescript"
-                        ],
-                        plugins: []
-                    }
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".ts", ".tsx"]
-    },
-    devtool: "eval-source-map"
+	entry: "./src/index.ts",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+		library: "TerminalEmulatorLib",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?(ts|tsx)$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-typescript"],
+						plugins: [],
+					},
+				},
+			},
+			{
+				test: /\.css$/i,
+				exclude: /(node_modules|bower_components)/,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
+	resolve: {
+		extensions: [".ts", ".tsx"],
+	},
+	devtool: "eval-source-map",
 };
